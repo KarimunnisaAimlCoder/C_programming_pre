31506b5cbfdc4abccbfe231cd3503a3c5328eadf
KarimunnisaAimlCoder <skkarimunnisa777@gmail.com>
2026-04-27 23:11:46 +0000

    Create factorial.c

---
+++ Day_2/factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+int fact=1;
+printf("Enter a number to find factorial");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+fact=fcat*i;//factorial logic
+}
+printf("factorial of an given %d is:%d",n,fact);//prints factorial
+}
