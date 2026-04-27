3fa0dada85423e7a15978b460f145aa1e66ff79a
KarimunnisaAimlCoder <skkarimunnisa777@gmail.com>
2026-04-27 23:15:26 +0000

    Update factorial.c

--- Day_2/factorial.c
+++ Day_2/factorial.c
@@ -5,9 +5,20 @@ int main()
 int fact=1;
 printf("Enter a number to find factorial");
 scanf("%d",&n);
+if(n<0)
+{
+  printf("Negative number has no factorila");
+}
+else if(n==0)
+{
+  printf("factorial of 0=1");
+}
+else
+{
 for(int i=1;i<=n;i++)
 {
 fact=fcat*i;//factorial logic
 }
 printf("factorial of an given %d is:%d",n,fact);//prints factorial
+}
 }
