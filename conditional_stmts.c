d9e072951ba72313f54d0c4f69cf2e1d2cc65832
KarimunnisaAimlCoder <skkarimunnisa777@gmail.com>
2026-04-27 14:19:00 +0530

    Create conditional_stmts.c

---
+++ conditional_stmts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int age;
+printf("Enter your age:");
+scanf("%d",&age);
+if(age>=18)://check the age if age is less than or equal to 18 then the person is not eligible for vote other wise eligible for vote
+{
+printf("Eligible for vote");//prints if persons age is greater than or equal to 18
+}
+else
+{
+print("Not eligible for vote");//print if person age is less than 18
+}
+}
