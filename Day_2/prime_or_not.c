b5da27b78d0175683dafc06e8acbb25a229374e0
KarimunnisaAimlCoder <skkarimunnisa777@gmail.com>
2026-04-27 23:26:00 +0000

    Create prime_or_not.c

---
+++ Day_2/prime_or_not.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,flag=1;
+printf("Enter a number:");
+scanf("%d",&n);
+if(n<=1)
+{
+flag=0;
+}
+else
+{
+for(i=2;i<n;i++)
+{
+if(n%1==0
+{
+flag=0;
+break;
+}
+}
+}
+if(flag==1)
+{
+printf("%d is prime number:",n);
+}
+else
+{
+printf("%d is not a prime number",n);
+}
+return 0;
+}
+
+pri
